ad354f5c9e17dc9e276f5e85356d9d4c12c195f1
Katie-Schwerer <katieschwerer@gmail.com>
2026-08-11 10:56:26 -0400

    starting to add some functionalies to the map.

--- graveyard/src/component/MapView.jsx
+++ graveyard/src/component/MapView.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect } from "react";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
-//import L from 'leaflet'
-//import ReactDOMServer from 'react-dom/server';
-//import SailingIcon from '@mui/icons-material/Sailing';
 
 import shipwrecks from "../data/shipwrecks.json";
 import ships from "../data/Shipwrecks-Media.json";
 
 import "leaflet/dist/leaflet.css";
-import "../App.css"
+import "../App.css";
 import ShipWreckMarker from "./ShipWreckMarker";
+
+import SailingIcon from "@mui/icons-material/Sailing";
+import WaterDropIcon from '@mui/icons-material/WaterDrop';
+import GasMeterIcon from '@mui/icons-material/GasMeter';
+import DirectionsBoatIcon from '@mui/icons-material/DirectionsBoat';
+import { RxColorWheel } from 'react-icons/rx';
+import { GiBattleship, GiBoatFishing, GiCargoCrane, GiCargoShip, GiFishingBoat, GiIronHulledWarship, GiPoliceBadge, GiSubmarine } from "react-icons/gi";
+import { FaLightbulb, FaShip } from "react-icons/fa";
+import { RiShip2Fill } from "react-icons/ri";
+import { SiCodeship, SiMentorcruise } from "react-icons/si";
+import { PiEngineBold } from "react-icons/pi";
 
 function InvalidateSize() {
   const map = useMap();
@@ -19,11 +27,40 @@ function InvalidateSize() {
   return null;
 }
 
+let typeAndIcons = [
+  {type: "Sailing Vessel", icon: <SailingIcon />},
+  {type: "Tanker", icon: <WaterDropIcon />},
+  {type: "Gas Stern Wheel", icon: <GasMeterIcon />},
+  {type: "Steamer", icon: <RxColorWheel />},
+  {type: "Trawler", icon: <GiFishingBoat />},
+  {type: "Barge", icon: <DirectionsBoatIcon />},
+  {type: "Lightship", icon: <FaLightbulb />},
+  {type: "Freighter", icon: <GiCargoShip />},
+  {type: "Tugboat", icon: <RiShip2Fill />},
+  {type: "Dredge", icon: <GiCargoCrane />},
+  {type: "Submarine", icon: <GiSubmarine />},
+  {type: "Fishing Vessel", icon: <GiBoatFishing />},
+  {type: "Yacht", icon: <SiMentorcruise />},
+  {type: "Motor Vessel Diesel", icon: <PiEngineBold />},
+  {type: "Gunboat", icon: <GiBattleship />},
+  {type: "Patrol Boat", icon: <GiPoliceBadge />},
+  {type: "Tank Landing Ship", icon: <SiCodeship />},
+  {type: "Ironclad", icon: <GiIronHulledWarship />},
+  {type: "Liberty Ship", icon: <FaShip />}
+]
+
 function MapView() {
 
   return (
-    <div className="map-comp" style={{ height: "93vh", width: "100%" }}>
-      <MapContainer center={[35.2909874203695, -75.6829703698898]} zoom={7} style={{ height: "93vh", width: "100%" }}>
+    <div className="map-comp">
+      <div className="map-filter">
+
+      </div>
+      <MapContainer
+        center={[35.2909874203695, -75.6829703698898]}
+        zoom={7}
+        style={{ height: "93vh", width: "100%" }}
+      >
         <InvalidateSize />
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
